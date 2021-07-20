8c3a49c9a7cbf8e759e3880e8144b55dc78d7c64
MaheSajib <mahe.sajib@gmail.com>
2021-07-20 23:42:58 +0600

    styling cards

--- src/components/Home/AboutMe/AboutMe.js
+++ src/components/Home/AboutMe/AboutMe.js
@@ -17,7 +17,7 @@ const AboutMe = () => {
                     <h5>Hello! <span className="about-name">I'm Md. Mahe Alam.</span></h5>
                     <p className="about-text">I'm a Fanatical Full-Stack Web Developer and dealing during this sector since 2019. <br /> I even have a Bachelor of Science in Computer Engineering and that I have also completed another Diploma in Digital Marketing.  <br /> Throughout my service period, I even have gained adequate experience to try to SEO, Affiliated Marketing, Social Media Marketing, etc. <br /> with the simplest possible efficiency. I'm currently working as a Web Developer and that I am going to be available to create an dynamic websites. </p>
                     <div className="icons-section d-flex justify-content-center my-3">
-                        <a target="_blank" href="https://www.facebook.com/rehan.rayhan"><FontAwesomeIcon className="social-icon" icon={faFacebook} /></a>
+                        <a target="_blank" href="https://www.facebook.com/mahealamsajib"><FontAwesomeIcon className="social-icon" icon={faFacebook} /></a>
                         <a target="_blank" href="https://www.linkedin.com/in/mahe-alam-/"><FontAwesomeIcon className="social-icon" icon={faLinkedin} /></a>
                         <a target="_blank" href="https://twitter.com/MaheSajib"><FontAwesomeIcon className="social-icon" icon={faTwitter} /></a>
                         <a target="_blank" href="https://github.com/MaheSajib"><FontAwesomeIcon className="social-icon" icon={faGithub} /></a>
